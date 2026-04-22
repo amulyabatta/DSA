813af1eabec19ed5b73d7678b1344f4761ca94b0
Amulya Batta <amulyabatta06@gmail.com>
2026-04-22 22:45:00 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (86.62%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -4,18 +4,15 @@ public:
         int n=nums.size();
         
         vector<int> res(n,0);
-        for(int i=0;i<n;i++){
-            nums[i]=nums[i]*nums[i];
-        }
         int left=0;
         int right=n-1;
         for(int i=n-1;i>=0;i--){
-            if(nums[left]>=nums[right]){
-                res[i]=nums[left];
+            if(abs(nums[left])>=abs(nums[right])){
+                res[i]=nums[left]*nums[left];
                 left++;
             }
             else{
-                res[i]=nums[right];
+                res[i]=nums[right]*nums[right];
                 right--;
             }
         }
