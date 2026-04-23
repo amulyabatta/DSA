972d847b1cccc8e0206ce948c4193eb080086f0d
Amulya Batta <amulyabatta06@gmail.com>
2026-04-23 17:33:34 +0530

    Time: 0 ms (100.00%) | Memory: 113.8 MB (60.16%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        int max_sum=sum;
+        int l=0,r=k;
+        for(int i=k;i<n;i++){
+            sum-=nums[l];
+            sum+=nums[r];
+            l++;
+            r++;
+            max_sum=max(max_sum,sum);
+        }
+        return static_cast<double>(max_sum)/k;
+    }
+};
